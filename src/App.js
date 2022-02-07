72eb2a78b1b5cedfab023b7cbe70fb88ee3d5168
Magda <meg.nowak27@gmail.com>
2022-02-07 21:25:03 +0100

    added loading method

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import "./App.css";
 
+function Heading() {
+  return (
+    <header className="music-app-header">
+      <h1>Music Player</h1>
+    </header>
+  );
+}
+
+function SongPlayer() {
+  return (
+    <>
+      <Heading />
+      <audio controls={true}>
+        <source src="https://examples.devmastery.pl/assets/audio/deadfro5h.mp3" />
+      </audio>
+    </>
+  );
+}
+
 function App() {
+  const isLoading = false;
+  const hasErrors = false;
+  const showPlayer = !isLoading && !hasErrors;
+  let message = null;
+  if (isLoading) {
+    message = "Loading...";
+  }
+  if (hasErrors) {
+    message = "Error occured...";
+  }
+
   return (
     <div className="App">
-      <header className="music-app-header">
-        <h1>Music Player</h1>
-        <audio controls>
-          <source src="https://examples.devmastery.pl/assets/audio/deadfro5h.mp3" />
-        </audio>
-      </header>
+      <section>{showPlayer ? <SongPlayer /> : message}</section>
     </div>
   );
+  
 }
 
 export default App;
