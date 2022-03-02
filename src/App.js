60182d2b3df1a8ff6b9d03bfe2a8e6e2bd5468ec
Magda <meg.nowak27@gmail.com>
2022-03-02 21:24:06 +0100

    added music list

--- src/App.js
+++ src/App.js
@@ -1,42 +1,73 @@
 import "./App.css";
 
-function Heading() {
-  return (
-    <header className="music-app-header">
-      <h1>Music Player</h1>
-    </header>
-  );
+function Heading({ title }) {
+  return <h1> {title} </h1>;
 }
 
-function SongPlayer() {
+function SongPlayer({ showControls = true, song }) {
+  const { audioUrl, coverUrl } = song;
   return (
-    <>
-      <Heading />
-      <audio controls={true}>
-        <source src="https://examples.devmastery.pl/assets/audio/deadfro5h.mp3" />
+    <section>
+      <Heading title="Music Player" />
+      <img width="250" height="250" src={coverUrl} alt="Song cover"></img>
+      <audio key={audioUrl} controls={showControls}>
+        <source src={audioUrl}></source>
       </audio>
-    </>
+    </section>
   );
 }
 
-function App() {
-  const isLoading = false;
-  const hasErrors = false;
-  const showPlayer = !isLoading && !hasErrors;
-  let message = null;
-  if (isLoading) {
-    message = "Loading...";
-  }
-  if (hasErrors) {
-    message = "Error occured...";
-  }
+function SongListItem({ song, isCurrent }) {
+  const backgroundColor = isCurrent ? "darkslategrey" : "inherit";
+  const color = isCurrent ? "white" : "inherit";
+  const style = { backgroundColor, color };
+  return (
+    <li style={style}>
+      {song.title} by {song.artist}
+    </li>
+  );
+}
+
+export default function App() {
+  const songs = [
+    {
+      audioUrl: "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3",
+      coverUrl: "https://examples.devmastery.pl/assets/audio/deadfro5h.jpg",
+      title: "Deadfro5h",
+      artist: "starfrosh",
+    },
+    {
+      audioUrl: "https://examples.devmastery.pl/assets/audio/majesty.mp3",
+      coverUrl: "https://examples.devmastery.pl/assets/audio/majesty.jpg",
+      title: "Majesty (Original Mix)",
+      artist: "Ryan Craig Martin",
+    },
+    {
+      audioUrl: "https://examples.devmastery.pl/assets/audio/runs.mp3",
+      coverUrl: "https://examples.devmastery.pl/assets/audio/runs.jpg",
+      title: "Runs",
+      artist: "Wowa",
+    },
+  ];
+  const currentSong = songs[0];
 
   return (
     <div className="App">
-      <section>{showPlayer ? <SongPlayer /> : message}</section>
+      <section>
+        <SongPlayer song={currentSong} />
+        <section>
+          <Heading title="Songs" />
+          <ul>
+            {songs.map((song) => (
+              <SongListItem
+                key={song.audioUrl}
+                song={song}
+                isCurrent={currentSong.audioUrl === song.audioUrl}
+              />
+            ))}
+          </ul>
+        </section>
+      </section>
     </div>
   );
-  
 }
-
-export default App;
